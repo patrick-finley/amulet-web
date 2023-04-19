447fb82c5c170a8c6c4a55cded2e2bea2aef5c40
patrick-finley <74550705+patrick-finley@users.noreply.github.com>
2023-04-19 21:22:21 +0000

    fixed frontend to make posts to upload and query

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,31 +10,56 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const [audioFile, setAudioFile] = useState<File | null>(null); // for the audio file
-  const [sumReq, setSumReq] = useState(''); // for the summary request
-  const [promptReq, setPromptReq] = useState(''); // for the prompt
-  const [result, setResult] = useState('');
+  const [audioFile1, setAudioFile1] = useState<File | null>(null); // for the audio file
+  const [audioFile2, setAudioFile2] = useState<File | null>(null); // for the audio file
+  const [audioFile3, setAudioFile3] = useState<File | null>(null); // for the audio file
+  const [audioFile4, setAudioFile4] = useState<File | null>(null); // for the audio file
+  const [audioFile5, setAudioFile5] = useState<File | null>(null); // for the audio file
+  const [query, setQuery] = useState(''); // for the prompt
+  const [response, setResponse] = useState('');
 
-  const handleClick = async (event: any) => {
+  const handleUpload = async (event: any) => {
     event.preventDefault()
-    console.log(sumReq + ' ' + promptReq + ' ahah')
 
     const formData = new FormData();
-    if (audioFile !== null) {
-      formData.append('audiofile', audioFile);
+    if (audioFile1 !== null) {
+      formData.append('audiofile1', audioFile1);
     }
-    formData.append('summary_request', sumReq);
-    formData.append('prompt', promptReq);
+    if (audioFile2 !== null) {
+      formData.append('audiofile2', audioFile2);
+    }
+    if (audioFile3 !== null) {
+      formData.append('audiofile3', audioFile3);
+    }
+    if (audioFile4 !== null) {
+      formData.append('audiofile4', audioFile4);
+    }
+    if (audioFile5 !== null) {
+      formData.append('audiofile5', audioFile5);
+    }
 
-    const response = await fetch('https://amulet-server.patrick-finley.repl.co/get_summary', {
+    const response = await fetch('https://amulet-server.patrick-finley.repl.co/upload', {
       method: 'POST',
       body: formData,
-      headers: { 'secret_value': 'val219' }
     });
 
-    const data = await response.text();
-    console.log(data);
-    setResult(data);
+    const cnfrm = await response.text();
+    console.log(cnfrm);
+  }
+  
+  const handleQuery = async (event: any) => {
+    event.preventDefault()
+
+    const formData = new FormData();
+    formData.append('query', query);
+
+    const response = await fetch('https://amulet-server.patrick-finley.repl.co/question', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const ans = await response.text();
+    setResponse(ans);
   }
 
   return (
@@ -49,44 +74,69 @@ export default function Home() {
       
       <main className={styles.main}>
         
+{/* Audio input form */}
         <form>
-          
           <label>
-            Audio file:
-            <input type="file" accept="audio/*" onChange={(e) => {
+            Only mp3 files please!
+            <br />
+            Audio file 1:
+            <input type="file" accept=".mp3" onChange={(e) => {
               const file = e.target.files?.[0];
               if (file) {
-                setAudioFile(file);
+                setAudioFile1(file);
               }
             }} />
+            <br />
+            Audio file 2:
+            <input type="file" accept=".mp3" onChange={(e) => {
+              const file = e.target.files?.[0];
+              if (file) {
+                setAudioFile2(file);
+              }
+            }} />
+            <br />
+            Audio file 3:
+            <input type="file" accept=".mp3" onChange={(e) => {
+              const file = e.target.files?.[0];
+              if (file) {
+                setAudioFile3(file);
+              }
+            }} />
+            <br />
+            Audio file 4:
+            <input type="file" accept=".mp3" onChange={(e) => {
+              const file = e.target.files?.[0];
+              if (file) {
+                setAudioFile4(file);
+              }
+            }} />
+            <br />
+            Audio file 5:
+            <input type="file" accept=".mp3" onChange={(e) => {
+              const file = e.target.files?.[0];
+              if (file) {
+                setAudioFile5(file);
+              }
+            }} />
+            
           </label>
-          
-          <br />
-          
+          <button onClick={handleUpload}>Upload Files</button>
+        </form>
+
+{/* Query input form */}
+        <form>
           <label>
-            What is your summary request?
+            What is your query?
             <br />
             <textarea onChange={(e) => {
-              setSumReq(e.target.value)
+              setQuery(e.target.value)
             }} />
           </label>
-          
-          <br />
-          
-          <label>
-            What prompt do you want to use?
-            <br />
-            <textarea onChange={(e) => {
-              setPromptReq(e.target.value)
-            }} />
-          </label>
-          
-          <button onClick={handleClick}>Submit</button>
-          
+          <button onClick={handleQuery}>Submit Query</button>
         </form>
 
         <div>
-          <p>{result}</p>
+          <p>{response}</p>
         </div>
 
       </main>
